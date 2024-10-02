19f0d2230233ae50d296fc5d8ec42d32757452a1
unknown <rahulreddypurmani123@gmail.com>
2024-10-02 15:48:35 +0530

    added updateproposal and check-username routes

--- app/config/database.py
+++ app/config/database.py
@@ -12,6 +12,11 @@ try:
     collection_projects = db["projects"]
     collection_timeline = db["timeline"]
     collection_users = db["users"]
+    collection_users.create_index(
+        [("first_name", 1), ("last_name", 1)],
+        unique=True,
+        name="firstname_lastname_unique"
+    )
     collection_mentors = db["mentors"]
     collection_ideas = db["ideas"]   
     collection_programs = db["programs"]  
--- app/routes/devlup_route.py
+++ app/routes/devlup_route.py
@@ -1,15 +1,10 @@
 from fastapi import APIRouter
 from config.database import collection_projects
-from schema.ProjectSchema import list_schema
 from models.Project import Project
 
 route = APIRouter()
 
-@route.get('/projects')
-async def get_projects():
-    projects = list_schema(collection_projects.find())
-    print(projects)
-    return projects
+
 
 @route.post('/project')
 async def create_project(project: Project):
